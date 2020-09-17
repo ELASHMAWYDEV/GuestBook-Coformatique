5c2f432aecef181e3179bbf62d245ba3ea89480d
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-17 13:14:16 +0200

    Added: MongoDB class. auth & messages structure built

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //All GET requests are to be ignored
 app.get("*", (req, res) => {
