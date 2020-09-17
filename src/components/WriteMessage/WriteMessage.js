c26b1502590b287c9e43afe70b52475c4a385dec
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-17 17:48:56 +0200

    Add: Popup box to add new message in WriteMessage component

--- src/components/WriteMessage/WriteMessage.js
+++ src/components/WriteMessage/WriteMessage.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./WriteMessage.scss";
 
+//images
+import cancelImage from "../../assets/img/cancel.svg";
+
 class WriteMessage extends Component {
-  state = {  
+  state = {
+    boxVisible: false,
+    isLoggedIn: true,
+  };
 
-  }
-  render() { 
-    return (  
+  toggleBox = () => {
+    this.setState({ boxVisible: !this.state.boxVisible });
+  };
+  render() {
+    return (
       <div className="write-message-container">
-        <button className="write-new-btn">Write a new message</button>
-        <div className="popup-box-container" style={{display: "none"}}>
-
-        </div>
+        <button className="write-new-btn" onClick={this.state.isLoggedIn && this.toggleBox}>Write a new message</button>
+        {this.state.boxVisible && (
+          <div className="popup-box-container" style={{ display: "" }}>
+            <div className="box">
+              <img src={cancelImage} alt="close" className="close-img" onClick={this.toggleBox}/>
+              <h3>Write a new message</h3>
+              <div className="input-container">
+                <label>Message</label>
+                <textarea placeholder="Message" />
+              </div>
+              <button className="save-btn">Save</button>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
- 
+
 export default WriteMessage;
