14ff4e6eece8c878417364bdc2caf97b7dd811ef
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-18 18:00:23 +0200

    removed domain property from sent cookie in login route

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -68,7 +68,6 @@ router.post("/", async (req, res) => {
       res.cookie("@access_token", accessToken, {
         maxAge: 86400 * 1000,
         httpOnly: true,
-        domain: process.env.CLIENT_DOMAIN,
         secure: process.env.NODE_ENV == "production" ? true : false,
       });
 
