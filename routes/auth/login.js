094bd974afa622e9c9aeef00ffc4f3a5ce047a0c
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-18 21:14:05 +0200

    removed security property from cookie in login route 'it depends on ssl'

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -68,7 +68,6 @@ router.post("/", async (req, res) => {
       res.cookie("@access_token", accessToken, {
         maxAge: 86400 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV == "production" ? true : false,
       });
 
       //delete password from user object
