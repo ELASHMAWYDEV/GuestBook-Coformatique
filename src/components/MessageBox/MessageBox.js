16477926f30a8035557f113d39dc44e2d88bf7e0
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-19 21:52:40 +0200

    Added: reply box

--- src/components/MessageBox/MessageBox.js
+++ src/components/MessageBox/MessageBox.js
@@ -21,9 +21,11 @@ class MessageBox extends Component {
     marked: false,
     boxShown: true,
     editBox: false,
+    replyBox: false,
     msg: this.props.message,
     success: [],
     errors: [],
+    reply: "",
   };
 
   static defaultProps = {
@@ -36,11 +38,16 @@ class MessageBox extends Component {
   };
 
   pressESC = (e) => {
-    if (e.keyCode == 27 && this.state.editBox) this.toggleBox();
+    if (e.keyCode == 27 && this.state.editBox) this.toggleEditBox();
+    if (e.keyCode == 27 && this.state.replyBox) this.toggleReplyBox();
   };
 
-  toggleBox = () => {
+  toggleEditBox = () => {
     this.setState({ editBox: !this.state.editBox });
+  };
+
+  toggleReplyBox = () => {
+    this.setState({ replyBox: !this.state.replyBox });
   };
 
   editMessage = async (e) => {
@@ -81,7 +88,6 @@ class MessageBox extends Component {
     }
   };
 
-
   deleteMessage = async () => {
     let response = await axios.post(
       `${API}/messages/delete`,
@@ -96,103 +102,154 @@ class MessageBox extends Component {
     } else {
       this.setState({ errors: data.errors });
     }
-  }
+  };
+
+  replyMessage = async (e) => {
+    e.preventDefault();
+    let response = await axios.post(
+      `${API}/messages/reply`,
+      {
+        message: this.state.msg,
+        reply: this.state.reply,
+      },
+      { withCredentials: true }
+    );
+
+    let data = await response.data;
+
+    if (data.success) {
+      this.setState({ success: data.messages, replyBox: false });
+    } else {
+      this.setState({ errors: data.errors });
+    }
+  };
 
   render() {
     let message = this.state.message;
     let msgTime = new Date(message.createdAt);
     return (
-        <>
-          {this.state.errors.length != 0 && (
-            <Notifier
-              messages={this.state.errors}
-              onDone={() => this.setState({ errors: [] })}
-            />
-          )}
-          {this.state.success.length != 0 && (
-            <Notifier
-              messages={this.state.success}
-              type={true}
-              onDone={() => this.setState({ success: [] })}
-            />
-          )}
-          {this.state.editBox && (
-            <div className="edit-box-container">
-              <div className="box">
-                <img
-                  src={cancelImage}
-                  alt="close"
-                  className="close-img"
-                  onClick={this.toggleBox}
-                />
-                <h3>Write a new message</h3>
-                <form onSubmit={this.editMessage}>
-                  <div className="input-container">
-                    <label>Message</label>
-                    <textarea
-                      placeholder="Message"
-                      onChange={(e) =>
-                        this.setState({
-                          msg: { ...this.state.msg, msg: e.target.value },
-                        })
-                      }
-                      value={this.state.msg.msg}
-                    />
+      <>
+        {this.state.errors.length != 0 && (
+          <Notifier
+            messages={this.state.errors}
+            onDone={() => this.setState({ errors: [] })}
+          />
+        )}
+        {this.state.success.length != 0 && (
+          <Notifier
+            messages={this.state.success}
+            type={true}
+            onDone={() => this.setState({ success: [] })}
+          />
+        )}
+        {this.state.replyBox && (
+          <div className="edit-box-container">
+            <div className="box">
+              <img
+                src={cancelImage}
+                alt="close"
+                className="close-img"
+                onClick={this.toggleReplyBox}
+              />
+              <h3>Reply to the message</h3>
+              <form onSubmit={this.replyMessage}>
+                <div className="input-container">
+                  <label>Your reply</label>
+                  <textarea
+                    placeholder="Your reply"
+                    onChange={(e) =>
+                      this.setState({
+                        reply: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <button className="save-btn" type="submit">Save</button>
+              </form>
+            </div>
+          </div>
+        )}
+        {this.state.editBox && (
+          <div className="reply-box-container">
+            <div className="box">
+              <img
+                src={cancelImage}
+                alt="close"
+                className="close-img"
+                onClick={this.toggleEditBox}
+              />
+              <h3>Edit message</h3>
+              <form onSubmit={this.replyMessage}>
+                <div className="input-container">
+                  <label>Message</label>
+                  <textarea
+                    placeholder="Message"
+                    onChange={(e) =>
+                      this.setState({
+                        msg: { ...this.state.msg, msg: e.target.value },
+                      })
+                    }
+                    value={this.state.msg.msg}
+                  />
+                </div>
+                <button className="save-btn">Save</button>
+              </form>
+            </div>
+          </div>
+        )}
+        <div
+          className={`msg-box ${this.state.marked && "marked-box"}${
+            !this.state.boxShown && " not-visible"
+          }`}
+        >
+          <div className="box-header">
+            <div className="username">{message.username}</div>
+            <div className="time">
+              {message.createdAt &&
+                `${msgTime.getHours()}:${msgTime.getMinutes()}  ${msgTime.getDate()}/${msgTime.getMonth()}/${msgTime.getFullYear()}`}
+            </div>
+          </div>
+          <div className="msg-text">{message.msg}</div>
+          {!this.props.read && (
+            <div className="bottom-container">
+              {!this.props.own ? (
+                <>
+                  <div
+                    className="mark-read"
+                    onClick={() => {
+                      this.markBox();
+                      this.readMessage();
+                    }}
+                  >
+                    <p>Mark read</p>
+                    <img src={doneImage} alt="Mark read" />
                   </div>
-                  <button className="save-btn">Save</button>
-                </form>
-              </div>
-            </div>
-          )}
-          <div className={`msg-box ${this.state.marked && "marked-box"}${!this.state.boxShown && " not-visible"}`}>
-            <div className="box-header">
-              <div className="username">{message.username}</div>
-              <div className="time">
-                {message.createdAt &&
-                  `${msgTime.getHours()}:${msgTime.getMinutes()}  ${msgTime.getDate()}/${msgTime.getMonth()}/${msgTime.getFullYear()}`}
-              </div>
-            </div>
-            <div className="msg-text">{message.msg}</div>
-            {!this.props.read && (
-              <div className="bottom-container">
-                {!this.props.own ? (
-                  <>
-                    <div
-                      className="mark-read"
-                      onClick={() => {
-                        this.markBox();
-                        this.readMessage();
-                      }}
-                    >
-                      <p>Mark read</p>
-                      <img src={doneImage} alt="Mark read" />
-                    </div>
-                    <div className="reply">
-                      <p>reply</p>
-                      <img src={replyImage} alt="reply" />
-                    </div>
-                  </>
+                  <div className="reply" onClick={this.toggleReplyBox}>
+                    <p>reply</p>
+                    <img src={replyImage} alt="reply" />
+                  </div>
+                </>
               ) : (
-                  <>
-                  <div className="edit" onClick={this.toggleBox}>
+                <>
+                  <div className="edit" onClick={this.toggleEditBox}>
                     <p>Edit</p>
                     <img src={editImage} alt="edit" />
                   </div>
                   <div className="delete" onClick={this.deleteMessage}>
                     <p>Delete</p>
                     <img src={deleteImage} alt="delete" />
-                    </div>
-                    </>
-                )}
-              </div>
-            )}{" "}
-            {this.state.marked && (
-              <div className="marked-overlay">
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-            )}
-          </div>
-        </>
+                  </div>
+                </>
+              )}
+            </div>
+          )}{" "}
+          {this.state.marked && (
+            <div className="marked-overlay">
+              <FontAwesomeIcon icon={faCheck} />
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
