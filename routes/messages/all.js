cc7a3145f51728eb7946bfbf61475c4e008e3566
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-19 12:39:13 +0200

    route to get all messages is done

--- routes/messages/all.js
+++ routes/messages/all.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../../db");
 
 
+router.post("/", (req, res) => {
 
+  let messages = await db.collection("messages").find({}).toArray();
+  
+  if (messages.length != 0) {
+    return res.json({
+      success: true,
+      messages
+    });
+  } else {
+    return res.json({
+      success: false,
+      errors: ["There are no messages to show at the moment"]
+    });
+  }
+
+})
 
 module.exports = router;
