f2dc4d2c425160cef15ac37a31dd01f9ff587093
ELASHMAWY DEV <contact@elashmawydev.com>
2020-09-19 12:40:25 +0200

    Added: try catch to all messages route

--- routes/messages/all.js
+++ routes/messages/all.js
@@ -4,18 +4,26 @@ const db = require("../../db");
 
 
 router.post("/", (req, res) => {
+  try {
 
-  let messages = await db.collection("messages").find({}).toArray();
-  
-  if (messages.length != 0) {
-    return res.json({
-      success: true,
-      messages
-    });
-  } else {
+    let messages = await db.collection("messages").find({}).toArray();
+    
+    if (messages.length != 0) {
+      return res.json({
+        success: true,
+        messages
+      });
+    } else {
+      return res.json({
+        success: false,
+        errors: ["There are no messages to show at the moment"]
+      });
+    }
+    
+  } catch (e) {
     return res.json({
       success: false,
-      errors: ["There are no messages to show at the moment"]
+      errors: [`Error occured: ${e.message}`]
     });
   }
 
